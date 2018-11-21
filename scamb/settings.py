7ef7f8dba617ab36ccdfb2c7592dcf352ac67560
Dick Grayson <possyon@gmail.com>
2018-11-21 02:19:13 -0200

    Altercao topbar

--- scamb/settings.py
+++ scamb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['scamb.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['scamb.herokuapp.com', 'localhost','127.0.0.1']
 
 
 # Application definition
