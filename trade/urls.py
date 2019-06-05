cb0a2afaf40088c751dac57dfd59d7b5fe4efb7a
Gui Mendes <gmendes.monteiro@hotmail.com>
2019-06-05 18:14:46 -0300

    Integração
    Integração com google books, criação de página de busca e melhora na página de troca

--- trade/urls.py
+++ trade/urls.py
@@ -3,12 +3,14 @@ from .views import mylist, listing, showtrade
 from .views import new_trade
 from .views import edit_trade
 from .views import delete_trade
+from .views import boowl
 
 urlpatterns = [
     path('list/<int:id>/', showtrade, name='showtrade'),
     path('trades/', listing, name='trades'),
     path('mylist/', mylist,name='mylist'),
     path('new/',new_trade, name='new_trade'),
+    path('boowl/', boowl, name='boowl'),
     path('update/<int:id>/',edit_trade, name= 'edit_trade'),
     path('delete/<int:id>/',delete_trade, name= 'delete_trade')
 ]
--- trade/views.py
+++ trade/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Trade, Profile
 from .forms import TradeForm
+import requests
 # Create your views here.
 
 
@@ -44,12 +45,10 @@ def mylist(request):
 
 
 @login_required()
-def new_trade(request):
+def new_trade(request):       
 
     user = request.user
     profile = Profile.objects.get(user=user)
-
-
     form_trade = TradeForm(request.POST or None, request.FILES or None, initial={'fk_profile': profile})
 
     if form_trade.is_valid():
@@ -60,6 +59,43 @@ def new_trade(request):
 
     return render(request, 'newtrade.html', {'form_trade': form_trade, 'user': user, 'profile': profile})
 
+
+@login_required()
+def boowl(request):
+
+    #https://www.googleapis.com/books/v1/volumes?q=assassins+creed
+    #user = {}
+    #if 'username' in request.GET:
+    #    username = request.GET['totalItems']
+    #    url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:%s' % username
+    #    response = requests.get(url)
+    #    user = response.json() **/
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    
+    if 'keyword' in request.GET:
+        book = {}
+        keyword = request.GET["keyword"]
+        keyword = keyword.replace(' ', '+')
+        url = 'https://www.googleapis.com/books/v1/volumes?q=:%s' % keyword
+        response = requests.get(url)
+        search = response.json()
+
+        items = search["totalItems"]
+
+        if items > 0:
+            items = search["totalItems"]
+            book = search["items"]
+            #book = search["items"][0]["volumeInfo"]
+            #authors = search["items"][0]["volumeInfo"]["authors"][0]
+            
+    else:
+        items = 0
+        book ='Digite um Valor'
+        authors='Digite um Valor'
+        pass
+
+    return render(request, 'boowl.html', {'items': items, 'book': book, 'user': user, 'profile': profile})
 
 @login_required()
 def edit_trade(request, id):
@@ -82,6 +118,10 @@ def edit_trade(request, id):
 
 @login_required()
 def delete_trade(request, id):
+    
+    user = request.user
+    profile = Profile.objects.get(user=user)
+
     trade = get_object_or_404(Trade, pk=id)
     form = TradeForm(request.POST or None, request.FILES or None, instance=trade)
 
@@ -89,4 +129,4 @@ def delete_trade(request, id):
         trade.delete()
         return redirect('mylist')
 
-    return render(request,'deleteconfirm.html', {'trade': trade})
+    return render(request,'deleteconfirm.html', {'trade': trade, 'user': user, 'profile': profile})
